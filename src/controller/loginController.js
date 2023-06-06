09ac65627ca7833c289462b98a70be63b741ce14
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-06 17:45:53 -0300

    .

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -50,7 +50,7 @@ router.post("/", (req, res) => {
         res.setHeader('Set-Cookie', [cookie]).status(200).send({
             success: true,
             message: "Logged",
-            cookie: cookie
+            cookie: token
           })
 
     })
