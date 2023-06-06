16e60360418dae98182ed5329d0ef878a4edbe71
Arthrok <98776585+Arthrok@users.noreply.github.com>
2023-06-06 15:39:04 -0300

    .

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ require("./config/MongoConfig.js")
 const cors = require("cors")
 const corsPort = {
     credentials: true,
-    origin: ["gorgeous-faun-efe088.netlify.app"],
+    origin: ["https://gorgeous-faun-efe088.netlify.app"],
 }
 app.use(cors(corsPort))
 //cookie
