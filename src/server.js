46a5598f16d6a49837f156e6f5e3beaa85bae359
Eric Silveira Gomes <es9694649@gmail.com>
2023-06-06 10:40:47 -0300

    '.'

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ require("./config/MongoConfig.js")
 const cors = require("cors")
 const corsPort = {
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["https://main--gorgeous-faun-efe088.netlify.app/"],
 }
 app.use(cors(corsPort))
 //cookie
